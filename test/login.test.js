fc222f402d89e4a8a690079c393f4f91f10dcbaf
Matheus-araujo-dev <matheus.araujodev13@gmail.com>
2025-07-23 18:27:59 -0300

    test: bloquear usuario apos 3 tentativas

--- test/login.test.js
+++ test/login.test.js
@@ -3,11 +3,15 @@ const request = require('supertest');
 const { expect } = require('chai')
 require('dotenv').config()
 const postLogin = require('../fixtures/postLogin.json')
+const { obterToken } = require('../helpers/autenticacao.js')
 
 
 
 describe('Login', () => {
     describe('POST /login', () => {
+        beforeEach(async () => {
+            await obterToken()
+        })
         it('Deve retornar sucesso com 200 quando usar credenciais válidas', async () => {
             const bodyLogin = {...postLogin}
             const response = await request(process.env.BASE_URL)
@@ -30,6 +34,23 @@ describe('Login', () => {
                 .send(bodyLogin)
 
             expect(response.status).to.equal(401);
+        })
+
+        it('Deve bloquear o usuário após 3 tentativas com senha incorreta', async () => {
+            const bodyLogin = {
+                'username': postLogin.username,
+                'password': 'senhaInvalida'
+            }
+
+            let response
+            for (let i = 0; i < 3; i++) {
+                response = await request(process.env.BASE_URL)
+                    .post('/login')
+                    .set('Content-Type', 'application/json')
+                    .send(bodyLogin)
+            }
+
+            expect(response.status).to.equal(423)
         })
 
     })
